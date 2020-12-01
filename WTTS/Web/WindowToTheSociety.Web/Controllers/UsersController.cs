9ef7128647131918fcc7fa184831a6895c0fe088
slavi044 <slavi044@abv.bg>
2020-12-01 16:04:56 +0200

    Add AddPicture

--- WTTS/Web/WindowToTheSociety.Web/Controllers/UsersController.cs
+++ WTTS/Web/WindowToTheSociety.Web/Controllers/UsersController.cs
@@ -19,10 +19,25 @@
 
         public IActionResult Profile()
         {
+            if (!this.User.Identity.IsAuthenticated)
+            {
+                return this.View("Error");
+            }
+
             string userId = this.userManager.GetUserId(this.User);
             UsersProfileViewModel viewModel = this.usersSurvice.GetById(userId);
 
             return this.View(viewModel);
+        }
+
+        public IActionResult AddPicture()
+        {
+            if (!this.User.Identity.IsAuthenticated)
+            {
+                return this.View("Error");
+            }
+
+            return this.View();
         }
     }
 }
