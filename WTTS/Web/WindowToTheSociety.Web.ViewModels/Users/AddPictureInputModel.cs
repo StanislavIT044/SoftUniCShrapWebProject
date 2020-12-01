1f2bf62af143f6047819f47be846f33968f5ef8c
slavi044 <slavi044@abv.bg>
2020-12-01 18:49:39 +0200

    Implement AddPicture

---
+++ WTTS/Web/WindowToTheSociety.Web.ViewModels/Users/AddPictureInputModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WindowToTheSociety.Web.ViewModels.Users
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.AspNetCore.Http;
+
+    public class AddPictureInputModel
+    {
+        [Required]
+        public IFormFile Picture { get; set; }
+
+        public string Type { get; set; }
+    }
+}
--- WTTS/Web/WindowToTheSociety.Web/Controllers/UsersController.cs
+++ WTTS/Web/WindowToTheSociety.Web/Controllers/UsersController.cs
@@ -1,7 +1,12 @@
 ﻿namespace WindowToTheSociety.Web.Controllers
 {
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+
     using WindowToTheSociety.Data.Models;
     using WindowToTheSociety.Services.Data;
     using WindowToTheSociety.Web.ViewModels.Users;
@@ -10,11 +15,13 @@
     {
         private readonly IUsersSurvice usersSurvice;
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public UsersController(IUsersSurvice usersSurvice, UserManager<ApplicationUser> userManager)
+        public UsersController(IUsersSurvice usersSurvice, UserManager<ApplicationUser> userManager, IWebHostEnvironment webHostEnvironment)
         {
             this.usersSurvice = usersSurvice;
             this.userManager = userManager;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Profile()
@@ -38,6 +45,34 @@
             }
 
             return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPicture(AddPictureInputModel input)
+        {
+            if (!input.Picture.FileName.EndsWith(".jpg"))
+            {
+                this.ModelState.AddModelError("Picture", "Invalid file type.");
+            }
+
+            if (input.Picture.Length > 10 * 1024 * 1024)
+            {
+                this.ModelState.AddModelError("Picture", "File too big.");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+
+            string filePath = this.webHostEnvironment.WebRootPath + $"/{input.Type}s" + $"/{this.userManager.GetUserId(this.User)}.jpg";
+
+            using (FileStream stream = new FileStream(filePath, FileMode.Create))
+            {
+                await input.Picture.CopyToAsync(stream);
+            }
+
+            return this.Redirect("Profile");
         }
     }
 }
