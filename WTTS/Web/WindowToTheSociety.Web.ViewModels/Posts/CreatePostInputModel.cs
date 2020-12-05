1f88568a8f55ee7f2ea161f5185d6c87b5b54104
slavi044 <slavi044@abv.bg>
2020-12-06 01:47:56 +0200

    Implement CreatePost inputModel controller and view

---
+++ WTTS/Web/WindowToTheSociety.Web.ViewModels/Posts/CreatePostInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WindowToTheSociety.Web.ViewModels.Posts
+{
+    using Microsoft.AspNetCore.Http;
+
+    public class CreatePostInputModel
+    {
+        public string Text { get; set; }
+
+        public IFormFile Photo { get; set; }
+    }
+}
---
+++ WTTS/Web/WindowToTheSociety.Web/Controllers/PostsController.cs
@@ -0,0 +1,27 @@
+﻿namespace WindowToTheSociety.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PostsController : Controller
+    {
+        public PostsController()
+        {
+
+        }
+
+        [Authorize]
+        public IActionResult CreatePost()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(string a)
+        {
+            return this.Redirect("/Users/Profile");
+        }
+    }
+}
